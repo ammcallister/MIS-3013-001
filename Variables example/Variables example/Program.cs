74049c03df7cd886037d5fea285834012549e971
Alex McAllister <ammcallister@ou.edu>
2020-01-22 12:34:23 -0600

    created a project for examples and variables

---
+++ Variables example/Variables example/Program.cs
@@ -0,0 +1,16 @@
+﻿//Alex McAllister
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
