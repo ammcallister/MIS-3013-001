05cfdda15e09540a754010d4cfa1c83facb2beff
Alex McAllister <ammcallister@ou.edu>
2020-01-22 12:54:31 -0600

    first update changes

--- Variables example/Variables example/Program.cs
+++ Variables example/Variables example/Program.cs
@@ -11,6 +11,31 @@ namespace Variables_example
     {
         static void Main(string[] args)
         {
+            //Create an string named firstName
+            string firstName;
+
+            // Assign a value to firstName (your name)
+            firstName = "Alex";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 27;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "McAllister";
+
+            string fullName = firstName + " " + lastName;
+
+
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber
+                                + "\nwhich equates to a crazy number of " + crazyNumber);
+        
+            
+            // MAKE IT DYNAMIC
+
+            Console.ReadKey();
+         
         }
     }
 }
