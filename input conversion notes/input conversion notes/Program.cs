a39e6375b72addfe1d8673cb18a6f3fe271ee7aa
Alex McAllister <ammcallister@ou.edu>
2020-01-27 13:11:25 -0600

    update

---
+++ input conversion notes/input conversion notes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_conversion_notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
