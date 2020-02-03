eeedba5916ccb211030b9e74719b5b6f8c1143a0
Alex McAllister <ammcallister@ou.edu>
2020-02-03 12:55:40 -0600

    update

---
+++ loops 2/loops 2/Program.cs
@@ -0,0 +1,31 @@
+﻿//AM
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What number do you want to count up to? >>");
+            int threshold = Convert.ToInt32(Console.ReadLine());
+            
+            
+            //could also use < 100 
+            for (int i = 1; i <= threshold; i++)
+            {
+                Console.WriteLine(i + " ");
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
